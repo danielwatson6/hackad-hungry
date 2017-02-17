c1a1c5f26be041d7c25a792faf6a5f4665db039c
NYUAD <nyuad@C2-LIB-AIR-10.local>
2017-02-17 20:44:56 +0400

    display campaigns

--- imports/ui/components/App.jsx
+++ imports/ui/components/App.jsx
@@ -4,17 +4,37 @@ import Campaigns from './Campaigns'
 
 //data for testing, should come from database
 let campaignList = [{
+    id: 1,
     name: 'somename',
     owner: 'owner1',
+    deadline: 'somedeadline',
     restaurant: 'somerestaurant',
     status: 'isOpen',
   },
   {
+    id: 2,
     name: 'somename2',
     owner: 'owner2',
+    deadline: "somedeadline2",
     restaurant: 'somerestaurant2',
     status: 'isOpen',
-  }]
+  },
+  {
+    id: 3,
+    name: 'somename3',
+    owner: 'owner3',
+    deadline: "somedeadline3",
+    restaurant: 'somerestaurant3',
+    status: 'isOpen',
+  },
+  {
+    id: 4,
+    name: 'somename4',
+    owner: 'owner4',
+    deadline: "somedeadline4",
+    restaurant: 'somerestaurant4',
+    status: 'isOpen',
+  },]
 
 // App component - represents the whole app
 export default class App extends Component {
--- imports/ui/components/Campaign.jsx
+++ imports/ui/components/Campaign.jsx
@@ -4,10 +4,11 @@ class Campaign extends React.Component {
 	render(){
 		const campaign = this.props
 		return (
-			<ul>
-				<li>{campaign.name}</li>
-				<li>{campaign.owner}</li>
-				<li>{campaign.restaurant}</li>
+			<ul className="campaign">
+				<li className="campaign_name">{campaign.name}</li>
+				<li className="campaign_owner">{campaign.owner}</li>
+				<li className="campaign_restaurant">{campaign.restaurant}</li>
+				<li className="campaign_deadline">{campaign.deadline}</li>
 			</ul>
 		);
 	}
