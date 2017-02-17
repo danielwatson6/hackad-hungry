679319e7e724241270acbbabad0fe5fe03fe9008
Daniel Watson <watsondaniel6@gmail.com>
2017-02-17 14:24:49 +0400

    Added bootstrap

---
+++ imports/api/utils.js
@@ -0,0 +1,29 @@
+import { Meteor } from 'meteor/meteor'
+import { Mongo } from 'meteor/mongo'
+import { check } from 'meteor/check'
+
+
+export class Collection extends Mongo.Collection {
+  constructor(name) {
+    super(name)
+    this.deny({
+      insert() { return true },
+      update() { return true },
+      remove() { return true },
+    })
+  }
+}
+
+export const Matchers = {
+  ID: Meteor.Collection.ObjectID,
+  
+  NonEmptyString: Match.Where((s) => {
+    check(s, String)
+    return s.length > 0
+  }),
+  
+  NonZeroNumber: Match.Where((n) => {
+    check(n, Number)
+    return n !== 0
+  })
+}
---
+++ imports/ui/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+ 
+// App component - represents the whole app
+export default class App extends Component {
+ 
+  render() {
+    return (
+      <nav className="navbar navbar-inverse bg-inverse">
+        <a href="/" className="navbar-brand">Hungry@NYUAD</a>
+      </nav>
+    );
+  }
+}
