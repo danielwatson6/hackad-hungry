34b81aab640524c394b3f275a2c98eca7f76ee2e
robwayne <robwaynegordon@gmail.com>
2017-04-17 09:22:04 +0400

    Created dev branch. Added SignIn.jsx and added dummy signin route. Added dummy layout for signin page.

--- imports/ui/routes.jsx
+++ imports/ui/routes.jsx
@@ -1,13 +1,17 @@
 import { render } from 'react-dom';
-import  { BrowserRouter as Router, Route, IndexRoute} from 'react-router-dom';
+import  { BrowserRouter as Router, Route, IndexRoute, browserHistory} from 'react-router-dom';
 import App from './components/App.jsx';
 import React from 'react';
+import SignIn from './components/SignIn.jsx';
 
 Meteor.startup(() => {
-  render(
-    <Router>
-      <Route path="/" component={App} />
-    </Router>,
+  render((
+    <Router history={browserHistory}>
+      <div>
+        <Route exact path="/" component={App} />
+        <Route exact path="/signin" component={SignIn} />
+      </div>
+    </Router>),
     document.getElementById('render-target')
   );
 });
